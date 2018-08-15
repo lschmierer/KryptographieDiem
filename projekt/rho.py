5c2ce2adf341e2743e8ca96493a766e38c1b0d12
Lukas Schmierer <lukas.schmierer@lschmierer.de>
2018-08-15 20:47:11 +0200

    remove seed 0

--- projekt/rho.py
+++ projekt/rho.py
@@ -50,10 +50,6 @@ def generiere_original_walk(g, h, F_r, n_s):
     if not isinstance(n_s, int):
         raise TypeError('Parameter n_s ist nicht vom Typ  int')
 
-    # So haben wir verlgeichbare (Pseudo)zufallszahlen
-    # Das hilft uns bei Vergleichbarkeit und Fehlersuche
-    random.seed(0)
-
     g_pre = []
     for _ in range(n_s):
         u = F_r.element(_randrange(F_r.modulus))
